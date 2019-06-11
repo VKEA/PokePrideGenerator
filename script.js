dd3c7125215cf869779008ea59fa10500f7e6cbd
Zt-freak <bnkho98@hotmail.com>
2019-06-11 18:59:59 +0200

    added requests to the pokemon website

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ function renderImage () {
   const ctx = canvas.getContext("2d");
 
   const img1 = loadImage('./flags/'+document.getElementById('flagselector').value, main);
-  const img2 = loadImage('./pokemon/'+document.getElementById('pokemonselector').value, main);
+  const img2 = loadImage(document.getElementById('imageURL').href, main);
 
   let imagesLoaded = 0;
   function main() {
@@ -17,7 +17,7 @@ function renderImage () {
           ctx.drawImage(img1, 0, 0, 500, 500);
 
           ctx.globalAlpha = 1;
-          ctx.drawImage(img2, (canvas.width-300)/2, (canvas.height-300)/2, 300, 300);
+          ctx.drawImage(img2, (canvas.width-350)/2, (canvas.height-350)/2, 350, 350);
       }
   }
 
@@ -32,9 +32,60 @@ function renderImage () {
 }
 
 /*
+* Get pokémon image
+*/
+function getImage () {
+  const requestForm = new XMLHttpRequest();
+  requestForm.open('GET', document.getElementById('pokemonselector').value, true);
+
+  requestForm.onload = function() {
+    if (requestForm.status === 200) {
+      const form = JSON.parse(requestForm.responseText);
+
+      let name = document.getElementById('pokemonselector').options[document.getElementById('pokemonselector').selectedIndex].text;
+      var splitStr = name.toLowerCase().split('-');
+      for (var i = 0; i < splitStr.length; i++) {
+
+          splitStr[i] = splitStr[i].charAt(0).toUpperCase() + splitStr[i].substring(1);     
+      }
+      splitStr.join('-');
+
+      const requestPokemonForm = new XMLHttpRequest();
+      requestPokemonForm.open('GET', form.pokemon.url, true);
+
+      requestPokemonForm.onload = function() {
+        const pokemonForm = JSON.parse(requestPokemonForm.responseText);
+
+        let dexNumber = pokemonForm.species.url.slice(42,-1);
+        for (;dexNumber.length < 3;) {
+          dexNumber = "0"+dexNumber;
+        }
+        console.log('https://assets.pokemon.com/assets/cms2/img/pokedex/detail/'+dexNumber+'.png');
+        document.getElementById('imageURL').href = ('https://assets.pokemon.com/assets/cms2/img/pokedex/detail/'+dexNumber+'.png');
+        document.getElementById('imageURL').innerText = ('https://assets.pokemon.com/assets/cms2/img/pokedex/detail/'+dexNumber+'.png');
+        renderImage();
+      }
+
+      requestPokemonForm.onerror = function() {
+        console.error('An error occurred fetching the JSON from ' + url);
+      };
+
+      requestPokemonForm.send();
+    } else {
+      // Reached the server, but it returned an error
+    }
+  }
+  
+  requestForm.onerror = function() {
+    console.error('An error occurred fetching the JSON from ' + url);
+  };
+  
+  requestForm.send();
+}
+
+/*
 * Populate pokémon drop-down
 */
-
 let dropdown = document.getElementById('pokemonselector');
 dropdown.length = 0;
 
@@ -56,7 +107,7 @@ request.onload = function() {
     for (let i = 0; i < data.results.length; i++) {
       option = document.createElement('option');
       option.text = data.results[i].name;
-      option.value = data.results[i].name+'.png';
+      option.value = data.results[i].url + '';
       dropdown.add(option);
     }
    } else {
